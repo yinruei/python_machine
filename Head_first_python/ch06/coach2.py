244cb9e54e896f707c319971fad70fa7c495b86b
蘇胤瑞 <yinruei@g.ncu.edu.tw>
2018-12-28 01:03:07 +0800

    update ch06

--- Head_first_python/ch06/coach2.py
+++ Head_first_python/ch06/coach2.py
@@ -20,6 +20,15 @@ def get_coach_data(filename):
         print('File error: ' +str(ioerr))
         return (None)
 
+# sarah = get_coach_data('sarah2.txt')
+# (sarah_name, sarah_dob) = sarah.pop(0),sarah.pop(0)
+# print(sarah_name + "'s fastest times are: "+str(sorted(set([sanitize(t) for t in sarah]))[0:3]))
+
 sarah = get_coach_data('sarah2.txt')
-(sarah_name, sarah_dob) = sarah.pop(0),sarah.pop(0)
-print(sarah_name + "'s fastest times are: "+str(sorted(set([sanitize(t) for t in sarah]))[0:3]))
+sarah_data = {}
+sarah_data['Name'] = sarah.pop(0)
+sarah_data['DOB'] = sarah.pop(0)
+sarah_data['Times'] = sarah
+
+print(sarah_data['Name'] + "'s fastest times are: "+str(sorted(set([sanitize(t) for t in sarah_data['Times']]))[0:3]))
+
